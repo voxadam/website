cea22c99e38cd7656c61f18835f36c1e90e3abea
Danijel Grabez <danijel@sprawsm.com>
2016-06-08 10:43:50 +0200

    added intro images (test samples) + javascript and added base for intro section

--- source/js/components/main.js
+++ source/js/components/main.js
@@ -28,13 +28,43 @@ $(document).ready(function() {
 	var menu = $('.page-nav__list');
 
 	$('#page-nav-button').click(function() {
-		menu.slideToggle(500);
+		menu.slideToggle(200);
 	});
 
 	$(window).on('resize', function(){     
 		if(!jQuery('#page-nav-button').is(':visible') && !menu.is(':visible')) {
 			menu.css({'display':''});   
 		}
+	});
+
+
+  
+
+  // ===========================================================================
+  //
+	// Image panning
+
+	function screenSize(){
+		if ($(window).width() >= 990) {
+			panning();
+		}
+	}
+
+	function panning(){
+		$('body').mousemove(function (e) {
+			var mousePosX = (e.pageX / $(window).width()) * 100;
+			// var mousePosY = (e.pageY / $(window).width()) * 100;
+			$('.bolt-image--top-front').css('left', (mousePosX / 30) + '%').delay(1800);
+			$('.bolt-image--top-back').css('right', (mousePosX / 60) + '%').delay(1800);
+			$('.bolt-image--bottom-front').css('left', (mousePosX / 30) + '%').delay(1800);
+			$('.bolt-image--bottom-back').css('left', (mousePosX / 60) + '%').delay(1800);
+		});
+	}
+
+	screenSize();
+
+	$(window).resize(function() {
+		screenSize();
 	});
 	
 }); // end document ready
